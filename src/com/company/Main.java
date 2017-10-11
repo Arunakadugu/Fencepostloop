5a4d8551a02644eb55b8c3bd434f362e5b319028
Aruna kadugu <arunakumari.7k@gmail.com>
2017-10-11 13:47:52 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Fencepost {
+
+    public static void main(String[] args) {
+        Scanner win = new Scanner(System.in);
+    int a = win.nextInt();
+	int counter = 0;
+	int sum = 0;
+	while(counter <= i){
+        sum += counter;
+        System.out.print("," +counter+ );
+	counter++;
+    }
+
+}
